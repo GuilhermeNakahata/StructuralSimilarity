544ba128aa626a38235bed91ea2755e153ab1b0d
guilhermenakahata <guilhermenakahata@gmail.com>
2020-08-31 12:02:54 -0300

    [GLHEMEH][31-08-2020] Validaçã abaixo de 3000

--- main.py
+++ main.py
@@ -8,22 +8,15 @@ from skimage.measure import compare_ssim
 # https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
 
 print("Iniciando Verificacao")
-for i in range(1,6):
-    for j in range (i+1,6):
-        print('Kengai/img_' + str(i) + '.jpg')
-        print('Kengai/img_' + str(j) + '.jpg')
-        imageA = cv2.imread('Kengai/img_' + str(i) + '.jpg')
-        imageB = cv2.imread('Kengai/img_' + str(j) + '.jpg')
+for i in range(1,101):
+    for j in range (i+1,101):
+        imageA = cv2.imread('Arruda/imagem' + str(i) + '.jpg')
+        imageB = cv2.imread('Arruda/imagem' + str(j) + '.jpg')
         grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
         grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
         err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
         err /= float(imageA.shape[0] * imageA.shape[1])
-        (score, diff) = compare_ssim(grayA, grayB, full=True)
-        fig, ax = plt.subplots(1,2, figsize=(15, 15), squeeze=False)
-        print(err)
-        print(score)
-        fig.suptitle("MSE: {:.2f}".format(err) + " SSIM: {:.2f}".format(score), fontsize=14)
-        ax[0][0].imshow(imageA)
-        ax[0][1].imshow(imageB)
-        plt.show()
+        if(err < 3000):
+            print("Imagens possivelmente iguais: " + str(i) + " com a imagem: " + str(j))
+            print("SSIM: {}".format(err))
 
